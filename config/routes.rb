6765838dcbccc5b4ec27a86a811480917acfbc06
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-08 18:59:32 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
+  get 'static_pages/about'
 
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
+  root 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
