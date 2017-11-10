f94b4d1c18fd4b8b89a5fa9894371e27641b33a8
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-10 20:07:32 +0300

    update product.rb file

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,9 @@ class Product < ApplicationRecord
 	has_many :orders
 end
 def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
+		if Rails.env.production?
+			Product.where("name ilike ?", "%#{search_term}%")
+		else
+			Product.where("name LIKE ?", "%#{search_term}%")
+		end
+	end
