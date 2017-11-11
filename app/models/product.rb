ad222863efb95754b78c73e82d6b2ab5b8ab986f
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-11 16:46:06 +0300

    CRUD revisit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comment
 end
 
 def self.search(search_term)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108204159) do
+ActiveRecord::Schema.define(version: 20171111132456) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "rating"
+    t.string "message"
+    t.datetime "created_at"
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
