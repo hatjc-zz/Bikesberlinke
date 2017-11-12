6742e45055e5d9fda6ca5d013ef69ad0823c35cd
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-12 12:03:38 +0300

    tweaks added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
-class Comment < ApplicationRecord
+class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
+
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+
+  after_commit { CommentUpdateJob.perform_later(self, @user) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,19 @@
-class Product < ApplicationRecord
+class Product < ActiveRecord::Base
 	has_many :orders
 	has_many :comments
-	def self.search(search_term)
-		if Rails.env.production?
-			Product.where("name ilike ?", "%#{search_term}%")
-		else
-			Product.where("name LIKE ?", "%#{search_term}%")
-		end
+
+	validates :name, :price, :image_url, :color, presence: true
+	validates :price, numericality: true
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
+
+	def average_rating
+		comments.average(:rating).to_f
 	end
 end
