8448ad8a4fb2e8a932f27ab4d442ff707ab0738b
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-14 00:33:45 +0300

    update controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,8 @@ class CommentsController < ApplicationController
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
-  end
+      end
+    end
 
   def destroy
   end
@@ -20,6 +21,6 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:user_id, :body, :rating)
     end
-  end
+
 end
 
