1e5289a57750230a54ff42e4290cc21a998f6779
JOSEPH HATENS <jayhatens@gmail.com>
2017-11-14 07:09:50 +0300

    admin role

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-  end
+  @comment = Comment.find(params[:id])
+  product = @comment.product
+  @comment.destroy
+  redirect_to product
+end
 
   private
 
